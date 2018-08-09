ed7ff1182d19b5327b373acad390cc035c33d121
Jordy Chetty <jxrrdy@gmail.com>
2018-08-10 00:18:50 +0200

    Added Platform 3
    Lilypad's serve as a way to jump onto the next platform. Another walkway added to allow player to jump back onto platform 2. Blockade added to platform 3 to prevent the players from progressing without finding weapons.
    
    Platform 3 is a large area and will be the point of first combat after the the rocks are destroyed.

---
+++ Dog Island v1 - Unity/Assets/Scripts/Cloud.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+	public GameObject cloudOne, cloudTwo, cloudThree, cloudFour, cloudFive;
+	public GameObject island;
+	public int numberOfClouds;
+
+
+	// Use this for initialization
+	void Start () {
+		GenerateClouds ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// generate a sky of clouds
+	void GenerateClouds() {
+		for (int i = 0; i < numberOfClouds; i++) {
+			int cloudType = Random.Range (0, 4);
+
+			switch (cloudType) {
+				
+			case 0:
+				{
+					break;
+				}
+
+			case 1:
+				{
+					break;
+				}
+
+			case 2:
+				{
+					break;
+				}
+
+			case 3:
+				{
+					break;
+				}
+
+			case 4:
+				{
+					break;
+				}
+			}
+
+		}
+	}
+}
