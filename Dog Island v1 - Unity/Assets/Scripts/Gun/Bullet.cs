8ceb9b0041f3fd93491c5284f35d66b0331f4088
mathivhahy <mrmathivhahy@gmail.com>
2018-08-12 07:46:07 +0200

    Added Destroyable script for objects that can be shot at and Bullet script

---
+++ Dog Island v1 - Unity/Assets/Scripts/Gun/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+
+	/*
+	 * instances
+	 */
+	public int damage = 1;
+
+
+
+
+	/*
+	 * methods
+	 */
+
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+        //Debug.Log("Hit "+ other.gameObject.name);
+
+        Destroyable destroyableObject = other.transform.GetComponent<Destroyable>();
+        if(destroyableObject != null)
+		{
+			// if object is hit, subtract bullet damage from the object's health
+			destroyableObject.LoseHealth(damage);
+			Destroy(gameObject);
+		}
+    }
+
+}
---
+++ Dog Island v1 - Unity/Assets/Scripts/Gun/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour 
+{
+	/*
+	 * instances
+	 */
+	public GameObject bulletEmitter;
+	public GameObject bullet;
+	public float bulletForce;
+
+	/*
+	 * methods
+	 */
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			GameObject tempBullet;
+			tempBullet = Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation) as GameObject;
+
+			Rigidbody bulletRigidbody;
+			bulletRigidbody = tempBullet.GetComponent<Rigidbody>();
+			bulletRigidbody.AddForce(transform.forward * bulletForce);
+
+			Destroy(tempBullet, 10.0f);
+
+		}	
+	}
+}
