c1f656a34070baee4e156676175c92a1f95516c7
mathivhahy <mrmathivhahy@gmail.com>
2018-08-12 12:04:34 +0200

    Refactored the whole project. Looks neater now. Jordy, you'll find your assests in the Imported-Unity-Assets folder, under Aesthetics

---
+++ Dog Island v1 - Unity/Assets/Scripts/Entity/DestroyableEntity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableEntity : MonoBehaviour 
+{
+
+	/*
+	 * instances
+	 */
+	public int health = 1;
+
+
+	void Start () 
+	{
+		
+	}
+	
+	/*
+	 * methods
+	 */
+
+	void Update () 
+	{
+		
+	}
+
+
+	// decrements the entity's health when it gets hit by a bullet
+	public void LoseHealth(int amount)
+	{
+		health -= amount;
+
+		if(health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Dog Island v1 - Unity/Assets/Scripts/Entity/Gun/Bullet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+
+	/*
+	 * instances
+	 */
+	public int damage = 1;
+
+	public GameObject bulletDestructEffect;
+
+
+
+
+	/*
+	 * methods
+	 */
+
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+        //Debug.Log("Hit "+ other.gameObject.name);
+
+        DestroyableEntity destroyableObject = other.transform.GetComponent<DestroyableEntity>();
+        if(destroyableObject != null)
+		{
+			// if object is hit, subtract bullet damage from the object's health
+			destroyableObject.LoseHealth(damage);
+
+		}
+
+		var effect = Instantiate(bulletDestructEffect, gameObject.transform.position,  Quaternion.identity);
+		Destroy(effect , 2.0f);
+		Destroy(gameObject);
+
+    }
+
+}
